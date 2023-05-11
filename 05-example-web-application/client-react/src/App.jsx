61dd8fad8856a76784cc4fed8b69715ffb0f47e9
paul <zeparasega@gmail.com>
2023-05-11 18:16:49 -0400

    change message

--- 05-example-web-application/client-react/src/App.jsx
+++ 05-example-web-application/client-react/src/App.jsx
@@ -36,7 +36,7 @@ function CurrentTime(props) {
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Hey Team! 👋</h1>
+      <h1>Hey Team from shipyard! 🚢</h1>
       <CurrentTime api="/api/golang/"/>
       <CurrentTime api="/api/node/"/>
       <ReactQueryDevtools initialIsOpen={false} />
