8e2214bd73fe2e115bcacfdaab111324023b07d6
AxelLofberg <axellofbergj@gmail.com>
2024-12-06 11:00:39 +0100

    updating docker v3

--- 05-example-web-application/client-react/src/App.jsx
+++ 05-example-web-application/client-react/src/App.jsx
@@ -36,7 +36,7 @@ function CurrentTime(props) {
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Hey Team! 👋</h1>
+      <h1>Hey Team from Shipyard! 👋🚢</h1>
       <CurrentTime api="/api/golang/"/>
       <CurrentTime api="/api/node/"/>
       <ReactQueryDevtools initialIsOpen={false} />
